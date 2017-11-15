102d88648b765f9ac8ca1733cee8bb1ba72a308c
Sunny <sunnyksharma1@gmail.com>
2017-11-15 17:09:21 -0500

    Add message flashing and styling
    update README
    update HTML
    add css
    add flashing to project.py

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Restaurant menus online app
 
 online app to create, read, update, and delete restaurants and their respective menus
+
+Dependencies:
+
+* python 2.7
+* sqlalchemy
+* sqlite
+* flask
--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -26,6 +26,7 @@ def newMenuItem(restaurant_id):
         newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("new menu item created")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id = restaurant_id)
@@ -39,6 +40,7 @@ def editMenuItem(restaurant_id, menu_id):
             item.name = request.form['name']
         session.add(item)
         session.commit()
+        flash("menu item edited")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
         return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = item)
@@ -50,11 +52,13 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(item)
         session.commit()
+        flash("menu item deleted")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
         return render_template('deletemenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = item)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
